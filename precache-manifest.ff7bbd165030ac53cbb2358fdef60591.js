261f6ce346f3e937d4a8d710cdad9fd5b8fe8188
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-18 18:28:43 +0200

    Updates

---
+++ precache-manifest.ff7bbd165030ac53cbb2358fdef60591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b81dda1062dfc7b10886254bb041e1",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "952e9f72009d85abc30f",
+    "url": "/todo-list-react/static/css/main.7f6c590d.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952e9f72009d85abc30f",
+    "url": "/todo-list-react/static/js/main.00adc927.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
