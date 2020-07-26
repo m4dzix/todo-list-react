797cdcdad2efac987ab14be9aa1d0bf683baab1e
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-26 13:46:43 +0200

    Adding new task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <label><input className="form__input" name="task" type="text" required/></label>
-    <button className="form__button">add task</button>
-  </form>
-);
+const Form = ({addNewTask}) => {
+  const [newTaskContent, setNewTaskContent] = useState("")
+  const onFormSubmit = (event)=>{event.preventDefault();
+  addNewTask(newTaskContent.trim());
+};
 
+return(
+<form className="form" onSubmit={onFormSubmit}>
+<label><input className="form__input" value={newTaskContent} name="task" type="text" onChange={(event) => setNewTaskContent(event.target.value)} required/></label>
+<button className="form__button">add task</button>
+</form>
+);
+};
 export default Form;
