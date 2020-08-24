f912d412e5d1b0401c1abf8c1cca14236677ae87
m4dzix <m4dzix@users.noreply.github.com>
2020-08-24 16:29:48 +0200

    Created custom hook - useTasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect,} from 'react';
+import React, {useState} from 'react';
+import {useTasks} from "./useTasks";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -8,56 +9,21 @@ import Main from "./Main";
 import Wrapper from "./Wrapper";
 import Subheader from "./Subheader";
 
-const getTasks = ()=> {
-  const tasksSavedInLocalStorage = localStorage.getItem("tasks");
-  return tasksSavedInLocalStorage ? 
-  JSON.parse(tasksSavedInLocalStorage) : [] ;
-};
-
-
 function App() {
   
   const [hideDoneTasks, setHideDoneTasks]=useState(false);
-  const [tasks, setTasks] = useState(getTasks);
-
-  useEffect(()=>{ 
-    localStorage.setItem ("tasks", JSON.stringify(tasks))
-  },[tasks]);
-
 
   const toggleHideDoneTask = () => {
     setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
   };
 
-  const removeTask = (id) => {setTasks(tasks =>
-     tasks.filter(task => task.id !== id));
-  };
-
-  const toggleDoneTask = (id) => {
-    setTasks(tasks => tasks.map(task => {
-    if (task.id === id){
-      return{...task, done: !task.done}
-    }
-    return task;
-  }));
-};
-
-const allTasksDone = () => {
-  setTasks(
-    tasks => tasks.map((task)=>({...task, done:true}))
-  );
-};
-
-const addNewTask = (newTaskContent, tasks)=>{
-  setTasks(tasks => [...tasks,
-  {content: newTaskContent,
-    done: false,
-    id: tasks.length? tasks[tasks.length-1].id + 1 : 1
-  },
-])
-
-};
-
+  const {
+    tasks,
+    removeTask,
+    toggleDoneTask,
+    allTasksDone,
+    addNewTask, 
+  } = useTasks();
 
 return (
   <Wrapper>
