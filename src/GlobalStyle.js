f56c281e660bd2f2850b5225d9495f97713187f4
Magdalena Checinski <madzix.p730@gmail.com>
2020-09-18 21:58:06 +0200

    Set the "Lato" font as default

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 html {
     box-sizing: border-box;
+    font-family: "Lato", sans-serif;
 }
 
 *, ::after, ::before {
