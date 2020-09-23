95a4158ae556d6936d555e9c4d0dd04fbd2bb4fb
Magdalena Checinski <madzix.p730@gmail.com>
2020-09-23 17:17:42 +0200

    Deleted unused folder

--- src/features/tasks/TasksPage/TasksButton/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-`;
-export const Info = styled.p`
-  font-size: 20px;
-`;
